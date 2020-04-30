8f32d406cde9b52fd240770b1ad776946a299643
aniloracz <aniloracz@gmail.com>
2020-04-30 19:32:38 -0300

    integro de nuevo y arreglo red, ojo no poner instanciador de sequencer en guiscript

--- index.js
+++ index.js
@@ -8,36 +8,50 @@ var express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
-var matrix;
+
 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/start.html');
 });
 app.get('/sequencer', (req, res) => {
-  res.sendFile(__dirname +'/sequencer.html');
- });
+  res.sendFile(__dirname + '/sequencer.html');
+});
 
 
 app.use('/tone', express.static(__dirname + '/node_modules/tone/build/'));
 app.use('/audios', express.static(__dirname + '/audios/'));
-app.use('/GUI',  express.static(__dirname + '/GUI/'));
+app.use('/GUI', express.static(__dirname + '/GUI/'));
 
 
 
-io.of('/sequencer').on('connection', (socket) => {
+var player = io.of('/sequencer');
 
-  
-    console.log('a user connected to room:'+ socket.handshake.query);
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    socket.on('toggleStep', (msg) => {
-        console.log('message: ' + msg);
-        socket.broadcast.emit('toggleStep', msg);
-      });
+player.on('connection', (socket) => {
 
+  //console.log('a user connected');
+  socket.join(socket.handshake.query.roomID);
+  socket.roomID = socket.handshake.query.roomID;
+  console.log('a user connected to room:' + socket.roomID );
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+
+
+  socket.on('toggleStep', (msg) => {
+    console.log('emit changes to room: '+socket.roomID );
+    socket.to(socket.roomID).emit('toggleStep', msg);
+    //player.emit('toggleStep', msg);
+  });
+
+
+
+});
+
+
+
+
 http.listen(PORT, () => {
   console.log('listening on *:3000');
 });
