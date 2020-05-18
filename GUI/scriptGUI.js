d9773224666622e20ac616e933a9fcb63e3a583b
aniloracz <aniloracz@gmail.com>
2020-05-18 19:39:45 -0300

    volume, privacy rooms, play cursor, invitado varios canales

--- GUI/scriptGUI.js
+++ GUI/scriptGUI.js
@@ -22,11 +22,11 @@
 
  var dialins1 = new Nexus.Dial('#dial-ins1',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins1.on('change',function(v) {
@@ -35,11 +35,11 @@
 
  var dialins2 = new Nexus.Dial('#dial-ins2',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins2.on('change',function(v) {
@@ -48,11 +48,11 @@
 
  var dialins3 = new Nexus.Dial('#dial-ins3',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins3.on('change',function(v) {
@@ -61,11 +61,11 @@
 
  var dialins4 = new Nexus.Dial('#dial-ins4',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins4.on('change',function(v) {
@@ -74,11 +74,11 @@
 
  var dialins5 = new Nexus.Dial('#dial-ins5',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins5.on('change',function(v) {
@@ -87,24 +87,26 @@
 
  var dialins6 = new Nexus.Dial('#dial-ins6',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins6.on('change',function(v) {
    console.log(v);
+   console.log(this);
+
  })
 
  var dialins7 = new Nexus.Dial('#dial-ins7',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins7.on('change',function(v) {
@@ -113,28 +115,35 @@
 
  var dialins8 = new Nexus.Dial('#dial-ins8',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialins8.on('change',function(v) {
    console.log(v);
  })
-
-
+var dialins = [];
+dialins.push(dialins1);
+dialins.push(dialins2);
+dialins.push(dialins3);
+dialins.push(dialins4);
+dialins.push(dialins5);
+dialins.push(dialins6);
+dialins.push(dialins7);
+dialins.push(dialins8);
  //---------------------------------------
  // DIALES - volumen
 
  var dialch1 = new Nexus.Dial('#dial-ch1',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch1.on('change',function(v) {
@@ -145,11 +154,11 @@
 
  var dialch2 = new Nexus.Dial('#dial-ch2',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch2.on('change',function(v) {
@@ -158,11 +167,11 @@
 
  var dialch3 = new Nexus.Dial('#dial-ch3',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch3.on('change',function(v) {
@@ -171,11 +180,11 @@
 
  var dialch4 = new Nexus.Dial('#dial-ch4',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step': 1,
+   'value': 5
  })
 
  dialch4.on('change',function(v) {
@@ -184,11 +193,11 @@
 
  var dialch5 = new Nexus.Dial('#dial-ch5',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch5.on('change',function(v) {
@@ -197,11 +206,11 @@
 
  var dialch6 = new Nexus.Dial('#dial-ch6',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch6.on('change',function(v) {
@@ -210,11 +219,11 @@
 
  var dialch7 = new Nexus.Dial('#dial-ch7',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch7.on('change',function(v) {
@@ -223,13 +232,23 @@
 
  var dialch8 = new Nexus.Dial('#dial-ch8',{
    'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-   'mode': 'relative', // "absolute" or "relative"
+   'mode': 'relative', // "relative" or "relative"
    'min': 0,
-   'max': 1,
-   'step': 0,
-   'value': 0
+   'max': 10,
+   'step':1,
+   'value': 5
  })
 
  dialch8.on('change',function(v) {
    console.log(v);
  })
+
+ var dialchs=[];
+ dialchs.push(dialch8);
+ dialchs.push(dialch7);
+ dialchs.push(dialch6);
+ dialchs.push(dialch5);
+ dialchs.push(dialch4);
+ dialchs.push(dialch3);
+ dialchs.push(dialch2);
+ dialchs.push(dialch1);
