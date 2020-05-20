b88a528632ec7abb9d84a4cd032b6a77e75e04d3
Antar Kuri <34176213+Antar74@users.noreply.github.com>
2020-05-20 18:47:45 -0300

    prueba portrait landscape detect

---
+++ portraitlandscape.js
@@ -0,0 +1,14 @@
+window.onresize = function (event) {
+  applyOrientation();
+}
+
+function applyOrientation() {
+  if (window.innerHeight < window.innerWidth) {
+   alert("You are now in 1111");
+  } else {
+    if (window.innerHeight < window.innerWidth) {
+   alert("You are now in 2222");
+  } else {
+    alert("You are now in 33333"); 
+  }
+}}
