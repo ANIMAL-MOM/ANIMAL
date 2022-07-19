7ff96587078d16df467beb470fbb416486a4ffe1
Antar Kuri <perklapoint@gmail.com>
2022-07-19 09:11:34 -0300

    borrar diales scriptGUI.js

--- GUI/scriptGUI.js
+++ GUI/scriptGUI.js
@@ -1,23 +1,4 @@
-//---------------------------------------
-// SEQUENCER - secuenciador de 8 canales y 16 pasos
 
-//NO PONER ESTO ACA
-/*var sequencer = new Nexus.Sequencer('#seq-animal',{
-  'size': [580,380],
-  'mode': 'toggle',
-  'rows': 8,
-  'columns': 16
- })
-
- sequencer.on('change',function(v) {
-   console.log(v);
- })
-
- sequencer.on('step',function(v) {
-   console.log(v);
- })
- */
- //---------------------------------------
  // DIALES - instrumentos
 
  var dialins1 = new Nexus.Dial('#dial-ins1',{
@@ -129,72 +110,6 @@
 
  //---------------------------------------
 
-//Mobile
-
-var mobdialins1 = new Nexus.Dial('#mob-dial-ins1',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins2 = new Nexus.Dial('#mob-dial-ins2',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins3 = new Nexus.Dial('#mob-dial-ins3',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins4 = new Nexus.Dial('#mob-dial-ins4',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins5 = new Nexus.Dial('#mob-dial-ins5',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins6 = new Nexus.Dial('#mob-dial-ins6',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins7 = new Nexus.Dial('#mob-dial-ins7',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
-var mobdialins8 = new Nexus.Dial('#mob-dial-ins8',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0.5,
-  'max': 1.5,
-  'step':0.1,
-  'value': 1
-})
 
 
 
@@ -307,83 +222,8 @@ var mobdialins8 = new Nexus.Dial('#mob-dial-ins8',{
    console.log(v);
  })
 
- ///MOBILE
 
 
-var mobdialch1 = new Nexus.Dial('#mob-dial-ch1',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch2 = new Nexus.Dial('#mob-dial-ch2',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch3 = new Nexus.Dial('#mob-dial-ch3',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch4 = new Nexus.Dial('#mob-dial-ch4',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch5 = new Nexus.Dial('#mob-dial-ch5',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch6 = new Nexus.Dial('#mob-dial-ch6',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch7 = new Nexus.Dial('#mob-dial-ch7',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-var mobdialch8 = new Nexus.Dial('#mob-dial-ch8',{
-  'interaction': 'vertical', // "radial", "vertical", or "horizontal"
-  'mode': 'relative', // "relative" or "relative"
-  'min': 0,
-  'max': 10,
-  'step':1,
-  'value': 5
-})
-
-
-mobdialch8.colorize("accent","#FF7809");
-mobdialch7.colorize("accent","#FF7809");
-mobdialch6.colorize("accent","#FF7809");
-mobdialch5.colorize("accent","#FF7809");
-mobdialch4.colorize("accent","#FF7809");
-mobdialch3.colorize("accent","#FF7809");
-mobdialch2.colorize("accent","#FF7809");
-mobdialch1.colorize("accent","#FF7809");
 dialch8.colorize("accent","#FF7809");
 dialch7.colorize("accent","#FF7809");
 dialch6.colorize("accent","#FF7809");
@@ -393,14 +233,6 @@ dialch3.colorize("accent","#FF7809");
 dialch2.colorize("accent","#FF7809");
 dialch1.colorize("accent","#FF7809");
 
-mobdialins8.colorize("accent","#5EC20A");
-mobdialins7.colorize("accent","#5EC20A");
-mobdialins6.colorize("accent","#5EC20A");
-mobdialins5.colorize("accent","#5EC20A");
-mobdialins4.colorize("accent","#5EC20A");
-mobdialins3.colorize("accent","#5EC20A");
-mobdialins2.colorize("accent","#5EC20A");
-mobdialins1.colorize("accent","#5EC20A");
 dialins8.colorize("accent","#5EC20A");
 dialins7.colorize("accent","#5EC20A");
 dialins6.colorize("accent","#5EC20A");
@@ -411,14 +243,6 @@ dialins2.colorize("accent","#5EC20A");
 dialins1.colorize("accent","#5EC20A");
 
 var dialchs=[];
-dialchs.push(mobdialch1);
-dialchs.push(mobdialch2);
-dialchs.push(mobdialch3);
-dialchs.push(mobdialch4);
-dialchs.push(mobdialch5);
-dialchs.push(mobdialch6);
-dialchs.push(mobdialch7);
-dialchs.push(mobdialch8);
 dialchs.push(dialch8);
 dialchs.push(dialch7);
 dialchs.push(dialch6);
@@ -437,11 +261,3 @@ dialins.push(dialins5);
 dialins.push(dialins6);
 dialins.push(dialins7);
 dialins.push(dialins8);
-dialins.push(mobdialins1);
-dialins.push(mobdialins2);
-dialins.push(mobdialins3);
-dialins.push(mobdialins4);
-dialins.push(mobdialins5);
-dialins.push(mobdialins6);
-dialins.push(mobdialins7);
-dialins.push(mobdialins8);
